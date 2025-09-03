eb8f49f66595ee0fb88fb0d9886cb4cfd25d5cad
gstfnsk <giulia.chimini@gmail.com>
2025-09-03 14:08:07 -0300

    add initial project

---
+++ PartyGame/PartyGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  PartyGameApp.swift
+//  PartyGame
+//
+//  Created by Giulia Stefainski on 03/09/25.
+//
+
+import SwiftUI
+
+@main
+struct PartyGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
