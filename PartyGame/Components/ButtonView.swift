7d7fe60abacda8a980eb6a9e63f3a691c9c5e7ef
gstfnsk <giulia.chimini@gmail.com>
2025-09-09 16:35:55 -0300

    create button component

---
+++ PartyGame/Components/ButtonView.swift
@@ -0,0 +1,27 @@
+//
+//  ButtonView.swift
+//  PartyGame
+//
+//  Created by Giulia Stefainski on 09/09/25.
+//
+
+import SwiftUI
+
+struct ButtonView: View {
+    
+    var title: String
+    var action: () -> Void
+    
+    var body: some View {
+        Button {
+            ()
+        } label: {
+            Text(title)
+                .frame(maxWidth: .infinity)
+        }
+    }
+}
+
+//#Preview {
+//    ButtonView(title:"Confirm vote" action:())
+//}
