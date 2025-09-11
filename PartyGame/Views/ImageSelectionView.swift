919c16571b7f8d6efe1774558294749f7dd85a4e
Rafael Toneto <rafaeltoneto@Rafaels-MacBook-Pro-2.local>
2025-09-11 14:53:54 -0300

    minor adjusts to LobbyView

---
+++ PartyGame/Views/ImageSelectionView.swift
@@ -0,0 +1,14 @@
+//
+//  ImageSelectionView.swift
+//  PartyGame
+//
+//  Created by Rafael Toneto on 11/09/25.
+//
+
+import SwiftUI
+
+struct ImageSelectionView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
