8c009253a044321d33f475422e7642077716ee37
Lorenzo Fortes <mrlorenzo1608@gmail.com>
2025-09-18 16:05:37 -0300

    Phrase view implemented

--- PartyGame/Views/PhraseView.swift
+++ PartyGame/Views/PhraseView.swift
@@ -17,94 +17,101 @@ struct PhraseView: View {
     
     let columns = [GridItem(.flexible())]
     
+    private var button: some View { ButtonHighFidelityView(image: "img-pencilSymbol", title: "confirmPhrase", action:{
+        print("Submitted phrase: \(selectedPhrase)")
+        viewModel.submitPhrase(phrase: selectedPhrase.text)
+        if viewModel.haveAllPlayersSubmitted {
+            nextScreen = true
+        }
+    }, enabled: true)}
+    
     var body: some View {
         ZStack{
-            Image("texture")
+            Image("img-textureI")
                 .resizable()
                 .scaledToFill()
                 .frame(minWidth: 0)
                 .edgesIgnoringSafeArea(.all)
-            VStack (spacing: 85){
-                Text("Component")
-                    .frame(alignment: .top)
-                    .font(.system(size: 64, weight: .bold, design: .default))
-                VStack(spacing: 64){
-                    VStack(spacing: 16){
+            VStack(spacing: 85){
+                VStack(spacing: 24){
+                    VStack(spacing: 5){
+                        Text("round1")
+                            .font(.system(size: 15, weight: .medium, design: .rounded))
+                            .foregroundStyle(.lilac)
+                            .frame(maxWidth: .infinity, alignment: .leading)
+                        
+                        HStack(spacing: 34){
+                            Text("submit a phrase")
+                                .font(.custom("DynaPuff-Medium", size: 28))
+                                .foregroundStyle(.ice
+                                    .shadow(.inner(color: .lilac, radius: 2, y: 3)))
+                                .frame(maxWidth: .infinity, alignment: .leading)
+                            TimerComponent(duration: 30.0)
+                        }
+                    }
+                    ProgressBarComponent(duration: 30.0)
+                }
+                .padding(.horizontal)
+                VStack (spacing: 177){
+                    VStack(spacing: 64){
                         VStack(spacing: 16){
-                            TextField(
-                                "write your own phrase here",
-                                text: $selectedPhrase.text)
-                            .foregroundStyle(.lilac)
-                            .foregroundStyle(.lilac)
-                            .padding(.vertical, 12)
-                            .padding(.horizontal, 16)
-                            .frame(maxWidth: .infinity, minHeight: 52, maxHeight: 52, alignment: .leading)
-                            .background(RoundedRectangle(cornerRadius: 26)
-                                .fill(Color.lighterPurple.shadow(.inner(color: .darkerPurple, radius: 2, y: 3))))
-                            
                             VStack(spacing: 16){
-                                HStack(spacing: 63){
-                                    Text("or choose one of ours:")
-                                        .foregroundStyle(.ice)
-                                        .fontWeight(.semibold)
-                                        .font(.system(size: 20, weight: .semibold, design: .rounded))
-                                    Button{
-                                        displayedPhrases = Array(Phrases.all.shuffled().prefix(3))
-                                    }label: {
-                                        Image("Dice")
-                                            .background(Circle()
-                                                .frame(width: 35, height: 36)
-                                                .foregroundStyle(Color.lilac))
+                                TextField(
+                                    "write your own phrase here",
+                                    text: $selectedPhrase.text)
+                                .foregroundStyle(.lilac)
+                                .foregroundStyle(.lilac)
+                                .padding(.vertical, 12)
+                                .padding(.horizontal, 16)
+                                .frame(maxWidth: .infinity, minHeight: 52, maxHeight: 52, alignment: .leading)
+                                .background(RoundedRectangle(cornerRadius: 26)
+                                    .fill(Color.lighterPurple.shadow(.inner(color: .darkerPurple, radius: 2, y: 3))))
+                                
+                                VStack(spacing: 16){
+                                    HStack(spacing: 63){
+                                        Text("or choose one of ours:")
+                                            .foregroundStyle(.ice)
+                                            .fontWeight(.semibold)
+                                            .font(.system(size: 20, weight: .semibold, design: .rounded))
+                                        Button{
+                                            displayedPhrases = Array(Phrases.all.shuffled().prefix(3))
+                                        }label: {
+                                            Image("Dice")
+                                                .background(Circle()
+                                                    .frame(width: 35, height: 36)
+                                                    .foregroundStyle(Color.lilac))
+                                            
+                                        }
                                         
                                     }
-                                    
-                                }
-                                LazyVGrid(columns: columns, spacing: 8){
-                                    ForEach(displayedPhrases, id: \.self) { phrase in
-                                        PhraseComponent(phrase: phrase,
-                                                        isSelected: phrase == selectedPhrase,
-                                                        onSelect: {
-                                            selectedPhrase = phrase
-                                        })
+                                    LazyVGrid(columns: columns, spacing: 8){
+                                        ForEach(displayedPhrases, id: \.self) { phrase in
+                                            PhraseComponent(phrase: phrase,
+                                                            isSelected: phrase == selectedPhrase,
+                                                            onSelect: {
+                                                selectedPhrase = phrase
+                                            })
+                                        }
+                                        
                                     }
-                                    
                                 }
+                                .padding(.vertical, 12)
+                                .padding(.horizontal)
+                                .background(RoundedRectangle(cornerRadius: 26).fill(Color.lighterPurple))
+                                
                             }
-                            .padding(.vertical, 12)
                             .padding(.horizontal)
-                            .background(RoundedRectangle(cornerRadius: 26).fill(Color.lighterPurple))
+                            .padding(.vertical)
+                            .background(GradientBackground())
                             
                         }
-                        .padding(.horizontal)
-                        .padding(.vertical)
-                        .background(GradientBackground()
-                                    /*.opacity(0.5)*/)
-                        
-                    }
-                    .padding(.horizontal)
-                    Button {
-                        print("Submitted phrase: \(selectedPhrase)")
-                        viewModel.submitPhrase(phrase: selectedPhrase.text)
-                        if viewModel.haveAllPlayersSubmitted {
-                            nextScreen = true
+                        .onAppear() {
+                            displayedPhrases = Array(Phrases.all.shuffled().prefix(3))
                         }
-                        
-                    }label: {
-                        Text("Component")
                     }
-                    .foregroundStyle(.primary)
-                    .background(
-                        RoundedRectangle(cornerRadius: 16)
-                            .fill(.tertiary)
-                            .frame(width: 356, height: 42)
-                    )
-                    .padding(.horizontal, 16)
-                    .padding(.vertical, 16)
-                    .onAppear() {
-                        displayedPhrases = Array(Phrases.all.shuffled().prefix(3))
-                        displayedPhrases = Array(Phrases.all.shuffled().prefix(3))
-                    }
+                    button
                 }
+                .padding(.horizontal)
             }
         }
         .background(Color.darkerPurple)
@@ -113,6 +120,7 @@ struct PhraseView: View {
             ImageSelectionView()
         }
     }
+    
 }
 
 struct GradientBackground: View {
@@ -129,7 +137,7 @@ struct GradientBackground: View {
     }
 }
 
-var button = ButtonHighFidelityView(image: "img-pencilSymbol", title: "confirmPhrase", action:{}, enabled: false)
+
 
 #Preview {
     PhraseView()
