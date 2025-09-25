ae803e56859d14f8c17170a5e487efa889bba142
gstfnsk <giulia.chimini@gmail.com>
2025-09-25 15:30:16 -0300

    implement final design

--- PartyGame/Views/VotingView.swift
+++ PartyGame/Views/VotingView.swift
@@ -30,6 +30,14 @@ struct VotingView: View {
         ImageSubmission(
             playerID: "", image: UIImage(named: "img-placeholder9x16")!.jpegData(compressionQuality: 1.0)!,
             submissionTime: Date()
+        ),
+        ImageSubmission(
+            playerID: "", image: UIImage(named: "img-placeholder9x16")!.jpegData(compressionQuality: 1.0)!,
+            submissionTime: Date()
+        ),
+        ImageSubmission(
+            playerID: "", image: UIImage(named: "img-placeholder9x16")!.jpegData(compressionQuality: 1.0)!,
+            submissionTime: Date()
         )
     ]
     
@@ -46,77 +54,116 @@ struct VotingView: View {
                     Text("round 1")
                         .font(.system(size: 15, weight: .medium, design: .rounded))
                         .foregroundStyle(.lilac)
-                        .frame(maxWidth: .infinity, alignment: .leading)
                     
                     HStack {
                         Text("vote for the best")
                             .font(.custom("DynaPuff-Medium", size: 28))
                             .foregroundStyle(.ice
                                 .shadow(.inner(color: .lilac, radius: 2, y: 3)))
-                            .frame(maxWidth: .infinity, alignment: .leading)
                         TimerComponent(duration: 30.0)
                     }
                     
                     ProgressBarComponent(duration: 30.0)
                 }
+                .padding(.top, 59)
                 .padding(.horizontal)
                 Spacer()
                 ScrollView {
                     VStack(alignment: .leading, spacing: 16) {
-                        Text("the phrase is:")
-                            .foregroundColor(.ice)
-                            .font(.headline)
-                        
-                        Text("\"\(phrase)\"").font(.custom("DynaPuff-Medium", size: 22))
-                            .foregroundStyle(.ice)
-                        
-                        PinterestLikeGrid($imageSubmissions, columns: 2, spacing: 16) { photo, index in
-                            Button {
-                                selectedImage = photo.id
-                            } label: {
-                                if let data = photo.image, let uiImage = UIImage(data: data) {
-                                    Image(uiImage: uiImage)
-                                        .resizable()
-                                        .scaledToFit()
-                                        .cornerRadius(28)
-                                        .overlay(RoundedRectangle(cornerRadius: 28)
-                                            .stroke(Color.white, lineWidth: 4))
-                                        .overlay(
-                                            RoundedRectangle(cornerRadius: 28)
-                                                .stroke(
-                                                    selectedImage == photo.id
-                                                    ? Color.lightGreen : Color.white,
-                                                    lineWidth: 4
-                                                )
-                                        )
-                                } else {
-                                    Rectangle()
-                                        .fill(Color.gray.opacity(0.3))
-                                        .overlay(Text("No Img"))
-                                        .cornerRadius(28)
+                        VStack (spacing: 16){
+                            Text("the phrase is:")
+                                .foregroundColor(.ice)
+                                .font(.headline)
+                            
+                            Text("\"\(phrase)\"").font(.custom("DynaPuff-Medium", size: 22))
+                                .foregroundStyle(.ice)
+                        }
+                        .padding(16)
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                        .background(RoundedRectangle(cornerRadius: 26).foregroundStyle(.lighterPurple))
+                        .padding([.leading, .trailing, .top], 16)
+                        VStack {
+                            PinterestLikeGrid($imageSubmissions, columns: 2, spacing: 16) { photo, index in
+                                Button {
+                                    selectedImage = photo.id
+                                } label: {
+                                    if let data = photo.image, let uiImage = UIImage(data: data) {
+                                        Image(uiImage: uiImage)
+                                            .resizable()
+                                            .scaledToFit()
+                                            .cornerRadius(28)
+                                            .overlay(RoundedRectangle(cornerRadius: 28)
+                                                .stroke(Color.white, lineWidth: 4))
+                                            .overlay(
+                                                RoundedRectangle(cornerRadius: 28)
+                                                    .stroke(
+                                                        selectedImage == photo.id
+                                                        ? Color.lightGreen : Color.lilac,
+                                                        lineWidth: 4
+                                                    )
+                                            )
+                                            .overlay(alignment: .topTrailing) {
+                                                if selectedImage == photo.id {
+                                                    Image("img-check")
+                                                        .resizable()
+                                                        .frame(width: 40, height: 40)
+                                                        .offset(x: 10, y: -10)
+                                                    
+                                                }
+                                            }
+                                    } else {
+                                        Rectangle()
+                                            .fill(Color.gray.opacity(0.3))
+                                            .overlay(Text("No Img"))
+                                            .cornerRadius(28)
+                                    }
                                 }
                             }
                         }
                         .frame(maxWidth: .infinity)
-                        
-                        if let selectedImage {
-                            ButtonView(image: "iconVoteButton", title: "confirm vote", titleDone: "vote confirmed", action: {
-                                goToNextRound = true
-                            }, state: .enabled)
-                        } else {
-                            ButtonView(image: "iconVoteButton", title: "confirm vote", titleDone: "vote confirmed", action: {
-                                goToNextRound = true
-                            }, state: .inactive)
-                        }
+                        .padding([.leading, .trailing, .bottom], 16)
+                        .padding(.top, 8)
                     }
+                    .background(GradientBackground())
                     .padding()
                     .frame(maxWidth: .infinity)
+//                    .padding(.bottom, 14)
+                    Spacer().frame(height: 100)
                 }
             }
             //            .onAppear {
             //                imageSubmissions = viewModel.submissions(for: phrase)
             //            }
             .navigationBarBackButtonHidden(true)
+            
+            VStack() {
+                Spacer()
+                if let selectedImage {
+                    ButtonView(image: "iconVoteButton", title: "confirm vote", titleDone: "vote confirmed", action: {
+                        goToNextRound = true
+                    }, state: .enabled)
+                } else {
+                    ButtonView(image: "iconVoteButton", title: "confirm vote", titleDone: "vote confirmed", action: {
+                        goToNextRound = true
+                    }, state: .inactive)
+                }
+            }
+            .padding(.bottom, 34)
+            .padding(.horizontal, 16)
+        }
+        .background(Color.darkerPurple)
+    }
+    
+    struct GradientBackground: View {
+        let gradientBackground = LinearGradient(
+            gradient: Gradient(colors: [.lilac.opacity(0.5), .lighterPink.opacity(0.5)]),
+            startPoint: .top,
+            endPoint: .bottom)
+
+        var body: some View {
+            RoundedRectangle(cornerRadius: 32)
+                .fill(gradientBackground
+                    .shadow(.inner(color: Color.lilac, radius: 2, x: 0, y: 5)))
         }
     }
     
