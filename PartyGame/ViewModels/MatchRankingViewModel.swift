b22bc471d48f3ba51a52b2d6011d9100f8e5a836
Lorenzo Fortes <mrlorenzo1608@gmail.com>
2025-10-01 17:30:42 -0300

    Match ranking view almost finished

--- PartyGame/ViewModels/MatchRankingViewModel.swift
+++ PartyGame/ViewModels/MatchRankingViewModel.swift
@@ -43,22 +43,5 @@ class MatchRankingViewModel {
             }
         }
     }
-    
-    func remainingPlayers(limit: Int = 3) -> [(Player, Int)] {
-        let ranking = service.gamePlayers.map { ($0, $0.votes) }
-            .sorted { $0.1 > $1.1 }
-        return Array(ranking.dropFirst(limit))
-    }
-    
-    private func loadAvatars(for players: [GKPlayer]) {
-        for p in players {
-            p.loadPhoto(for: .small) { [weak self] img, _ in
-                guard let self else { return }
-                DispatchQueue.main.async {
-                    self.avatarByID[p.gamePlayerID] = img
-                }
-            }
-        }
-    }
 
 }
