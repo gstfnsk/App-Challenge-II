70f7eef28d62b7619ff103d0e64d96d434a03823
Rafael Toneto <rafaeltoneto@Rafaels-MacBook-Pro-2.local>
2025-10-02 14:18:16 -0300

    Unit tests Target added to project

---
+++ PicktureTests/PicktureTests.swift
@@ -0,0 +1,16 @@
+//
+//  PicktureTests.swift
+//  PicktureTests
+//
+//  Created by Rafael Toneto on 02/10/25.
+//
+
+import Testing
+
+struct PicktureTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
